3a252398548b04d8f24fdbd7e1090c80ac650b5e
Pasha <nikolaevpasa4@gmail.com>
2023-04-16 17:21:11 +0300

    Adding a function which reversing a sentanc

---
+++ src/main/kotlin/Test_String_revers.kt
@@ -0,0 +1,15 @@
+fun main(){
+    print("Write a sentanc : ")
+    var input : String = readln()
+    print("The result - " + ReversString(input))
+}
+fun ReversString(str_veriable:String): String {
+    var revers = ""
+    var counter_varieble = str_veriable.length - 1
+
+    while (counter_varieble >= 0) {
+        revers += str_veriable[counter_varieble]
+        counter_varieble--
+    }
+    return revers
+    }

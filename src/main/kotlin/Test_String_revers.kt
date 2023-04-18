3f3cd64c3eb6db61fdc70170c19ba05de28a33e0
Pasha <nikolaevpasa4@gmail.com>
2023-04-18 17:11:27 +0300

    Add new solution for function ReversString

--- src/main/kotlin/Test_String_revers.kt
+++ src/main/kotlin/Test_String_revers.kt
@@ -4,12 +4,12 @@ fun main(){
     print("The result - " + ReversString(input))
 }
 fun ReversString(str_veriable:String): String {
-    var revers = ""
-    var counter_varieble = str_veriable.length - 1
-
-    while (counter_varieble >= 0) {
-        revers += str_veriable[counter_varieble]
-        counter_varieble--
-    }
-    return revers
+//    var revers = ""
+//    var counter_varieble = str_veriable.length - 1
+//    while (counter_varieble >= 0) {
+//        revers += str_veriable[counter_varieble]
+//        counter_varieble--
+//    }
+//    return revers
+    return str_veriable.reversed()
     }
